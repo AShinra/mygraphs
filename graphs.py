2f173a6a20e4b6465cf8b162a3e7fc145bceadb9
Athan <jonpuray@gmail.com>
2025-02-26 15:18:32 +0800

    testing

--- graphs.py
+++ graphs.py
@@ -71,8 +71,8 @@ def bar_graph(df):
         # Save the Workbook to a file
         wb.save("Bar.xlsx")
 
-        excel_stream = BytesIO()
-        wb.save(excel_stream)
-        excel_stream.seek(0)
+        result_file = open("Bar.xlsx", 'rb')
+        st.success(f':red[NOTE:] Downloaded file will go to the :red[Downloads Folder]')
+        st.download_button(label='📥 Download Cleaned Raw', data=result_file ,file_name= f'testing_graph.xlsx')
 
     return
