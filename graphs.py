9beb5443c17b8f1fc6167b782c13752fc89ae2e0
Athan <jonpuray@gmail.com>
2025-02-26 15:13:59 +0800

    testing

---
+++ graphs.py
@@ -0,0 +1,78 @@
+import openpyxl
+import pandas as pd
+import streamlit as st
+from openpyxl.chart import BarChart, Reference
+from openpyxl.chart.label import DataLabelList
+from openpyxl.chart.plotarea import DataTable
+from io import BytesIO
+from PIL import Image
+
+
+def bar_graph(df):
+
+    # Create a new Workbook
+    wb = openpyxl.Workbook()
+    wb.create_sheet('Bar')
+    ws = wb['Bar']
+    wb.save('Bar.xlsx')
+
+    btn_create_graph = st.button('Create Graph')
+
+    
+    if btn_create_graph:
+        
+        l, w = df.shape
+
+        # append data to excel sheet
+        writer = pd.ExcelWriter('Bar.xlsx', engine='openpyxl')
+        df.to_excel(writer, sheet_name='Bar', startrow = 0,index = False)
+        writer.close()
+
+        wb = openpyxl.load_workbook('Bar.xlsx')
+        ws = wb['Bar']
+
+        # Create a BarChart object
+        _chart = BarChart()
+        _chart.type = 'col'
+        _chart.style = 2
+        _chart.grouping = 'stacked'
+        _chart.overlap = 100
+        _chart.gapWidth = 10
+        # _chart.x_axis.delete = False
+        # _chart.y_axis.delete = False
+        _chart.legend.position = 'b'
+
+        # Define the data (sales values) for the chart
+        data = Reference(ws, min_col=2, min_row=1, max_row=l+1, max_col=w)
+
+        # Define the categories (years)
+        categories = Reference(ws, min_col=1, min_row=2, max_row=l+1, max_col=1)
+
+        # Add data and categories to the chart
+        _chart.add_data(data, titles_from_data=True)
+        _chart.set_categories(categories)
+        _chart.shape = 4
+        _chart.DataTable = DataTable(True, True, True, True)
+        _chart.height = 15
+        _chart.width = 30
+
+        _chart.title = "Custom Bar Chart"
+        _chart.y_axis.title = "Values"
+        _chart.x_axis.title = "Categories"
+        # _chart.y_axis.majorGridlines = None
+        # _chart.x_axis.majorGridlines = None
+        # _chart.y_axis.tickLblPos = "low"
+        # _chart.x_axis.tickLblPos = "low"
+
+
+        # Add the chart to the worksheet
+        ws.add_chart(_chart, "J1")
+
+        # Save the Workbook to a file
+        wb.save("Bar.xlsx")
+
+        excel_stream = BytesIO()
+        wb.save(excel_stream)
+        excel_stream.seek(0)
+
+    return
---
+++ main.py
@@ -0,0 +1,38 @@
+import openpyxl
+import pandas as pd
+import streamlit as st
+from graphs import bar_graph
+from streamlit_option_menu import option_menu
+
+
+def get_data(csv_file):
+    return pd.read_csv(csv_file)
+
+
+
+if __name__ == '__main__':
+
+    with st.sidebar:
+       selected = option_menu(
+           menu_title='Graph Type',
+           menu_icon='map',
+           options=['Bar', 'Pie', 'Line'],
+           icons=['bar-chart-line', 'pie-chart', 'graph-up']
+       ) 
+
+    st.file_uploader('Upload File', type='csv', key='csvfile')
+
+    if st.session_state['csvfile'] not in [None, '']:
+        df = get_data(st.session_state['csvfile'])
+
+        with st.container(border=True):
+            st.dataframe(df, use_container_width=True)
+
+            if selected == 'Bar':
+                bar_graph(df)
+            
+            if selected == 'Pie':
+                st.warning('Under Development')
+
+            if selected == 'Line':
+                st.warning('Under Development')
